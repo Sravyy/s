4e1200c53d28001592f8ed5e52a5cf9a2e37af59
Hansen Hansen and sravanthi velaga <hansen@>
2017-09-13 09:43:37 -0700

    complete shouting machine html page and uppercase javascript

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+      var sentenceInput = $("input#sentence").val();
+      var upper = sentenceInput.toUpperCase();
+      $("#text").text(upper);
+
+      event.preventDefault();
+
+  })
+})
